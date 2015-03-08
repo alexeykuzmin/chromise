e5eead76060b3f4cfd34cdcff8e5924abb3270bb
Alexey Kuzmin <alex.s.kuzmin@gmail.com>
2015-03-08 20:21:41 +0300

    Light refactoring

--- chromise.js
+++ chromise.js
@@ -79,93 +79,86 @@
   }
 
 
+  let wrapGuy = {
+    /**
+     * @param {!Object} api API object to wrap.
+     * @param {!Object} exportTo
+     */
+    wrapApi(api, exportTo) {
+      wrapGuy.wrapObject_(exportTo, api);
+    },
+
+    /**
+     * Returns true if |apiEntry| is an API Event object,
+     * returns false otherwise.
+     * @param {!Object} apiEntry
+     * @return {boolean}
+     * @private
+     */
+    isApiEvent_(apiEntry) {
+      return apiEntry instanceof global.chrome.Event;
+    },
+
+    /**
+     * Returns true if function with name |methodName| looks like constructor,
+     * returns false otherwise.
+     * @param {string} methodName
+     * @return {boolean}
+     * @private
+     */
+    isConstructor_(methodName) {
+      let firstLetter = methodName[0];
+      return firstLetter == firstLetter.toUpperCase();
+    },
+
+    /**
+     * Adds all fields of |apiObject| to |exportTo| object.
+     * @param {!Object} exportTo
+     * @param {!Object} apiObject
+     * @private
+     */
+    wrapObject_(exportTo, apiObject) {
+      Object.keys(apiObject).forEach(function(keyName) {
+        let apiEntry = apiObject[keyName];
+        let entryType = typeof apiEntry;
+
+        if (entryType == 'function')
+          wrapGuy.wrapMethod_(exportTo, apiObject, keyName);
+        else if (entryType == 'object' && !wrapGuy.isApiEvent_(apiEntry))
+          wrapGuy.wrapObject_(exportTo[keyName] = {}, apiEntry);
+        else
+          exportTo[keyName] = apiEntry;
+      });
+    },
+
+    /**
+     * Adds new method to |exportTo| object with |methodName| name.
+     * @param {!Object} exportTo
+     * @param {!Object} apiObject
+     * @param {string} methodName
+     * @private
+     */
+    wrapMethod_(exportTo, apiObject, methodName) {
+      let originalMethod = apiObject[methodName];
+      let wrappedMethod = wrapGuy.isConstructor_(methodName) ?
+          originalMethod :
+          function() {
+            return new ApiCall(apiObject, originalMethod, arguments);
+          };
+
+      exportTo[methodName] = wrappedMethod;
+    }
+  };
+
+
   let chromise = {};
-
-
-  /**
-   * List of Extension API namespaces to wrap.
-   * @type {Array.<Object>}
-   * @private
-   */
-  chromise.API_NAMESPACES_ = [
-    global.chrome
-  ];
-
-
-  /**
-   * @private
-   */
-  chromise.init_ = function() {
-    let wrap = chromise.wrapObject_.bind(null, chromise);
-    chromise.API_NAMESPACES_.forEach(wrap);
+  chromise._ = {
+    ApiCall,
+    wrapGuy
   };
-
-
-  /**
-   * Return true if |apiEntry| is an API Event object, returns false otherwise.
-   * @param {!Object} apiEntry
-   * @return {boolean}
-   * @private
-   */
-  chromise.isApiEvent_ = function(apiEntry) {
-    return apiEntry instanceof global.chrome.Event;
-  };
-
-
-  /**
-   * Returns true if function with name |methodName| looks like constructor,
-   * returns false otherwise.
-   * @param {string} methodName
-   * @return {boolean}
-   * @private
-   */
-  chromise.isConstructor_ = function(methodName) {
-    let firstLetter = methodName[0];
-    return firstLetter == firstLetter.toUpperCase();
-  };
-
-
-  /**
-   * Adds all fields of |apiObject| to |exportTo| object.
-   * @param {!Object} exportTo
-   * @param {!Object} apiObject
-   * @private
-   */
-  chromise.wrapObject_ = function(exportTo, apiObject) {
-    Object.keys(apiObject).forEach(function(keyName) {
-      let apiEntry = apiObject[keyName];
-      let entryType = typeof apiEntry;
-
-      if (entryType == 'function')
-        chromise.wrapMethod_(exportTo, apiObject, keyName);
-      else if (entryType == 'object' && !chromise.isApiEvent_(apiEntry))
-        chromise.wrapObject_(exportTo[keyName] = {}, apiEntry);
-      else
-        exportTo[keyName] = apiEntry;
-    });
-  };
-
-
-  /**
-   * Adds new method to |exportTo| object with |methodName| name.
-   * @param {!Object} exportTo
-   * @param {!Object} apiObject
-   * @param {string} methodName
-   * @private
-   */
-  chromise.wrapMethod_ = function(exportTo, apiObject, methodName) {
-    let originalMethod = apiObject[methodName];
-    let wrappedMethod = chromise.isConstructor_(methodName) ?
-        originalMethod :
-        function() {
-          return new ApiCall(apiObject, originalMethod, arguments);
-        };
-
-    exportTo[methodName] = wrappedMethod;
-  };
+  wrapGuy.wrapApi(global.chrome, chromise);
 
 
   global.chromise = chromise;
-  chromise.init_();
 
 }(window));
